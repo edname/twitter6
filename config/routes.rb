e4ffaf1398d70467506f19bae34ba87c8864a5e4
Edward <lt93970@gmail.com>
2022-01-24 12:21:52 +0200

    devise trackable

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   
+  resources :users
+
   resources :posts do
     resources :comments
   end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "posts#index"
   
