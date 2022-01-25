6a64ba9b5b1ff345a6d0a52df2775fe97067246a
Edward <lt93970@gmail.com>
2022-01-25 08:34:40 +0200

    devise trackable and validatable added

---
+++ app/controllers/users/confirmations_controller.rb
@@ -0,0 +1,9 @@
+
+
+class Users::ConfirmationsController < Devise::ConfirmationsController
+    private
+    def after_confirmation_path_for(resource_name, resource)
+      sign_in(resource) # In case you want to sign in the user
+      users_path
+    end
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :trackable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :trackable, :confirmable,
          :recoverable, :rememberable, :validatable
          
 end
